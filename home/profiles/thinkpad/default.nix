39f45aadecf1204d00a8a528722b54215595e245
Icey-Glitch <67936038+Icey-Glitch@users.noreply.github.com>
2024-02-07 10:28:53 -0800

    think

--- home/profiles/thinkpad/default.nix
+++ home/profiles/thinkpad/default.nix
@@ -11,18 +11,10 @@
 
     # services
     ../../services/waybar
-    ../../services/cinny.nix
-
-    # media services
-    ../../services/media/playerctl.nix
-    ../../services/media/spotifyd.nix
 
     # system services
-    ../../services/system/dunst.nix
-    ../../services/system/kdeconnect.nix
     ../../services/system/polkit-agent.nix
     ../../services/system/power-monitor.nix
-    ../../services/system/syncthing.nix
     ../../services/system/udiskie.nix
 
     # wayland-specific
