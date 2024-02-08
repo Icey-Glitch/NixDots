eefdcb2298a029305f3ad562b51e58f2b5ab5f51
Icey-Glitch <67936038+Icey-Glitch@users.noreply.github.com>
2024-02-08 08:20:30 -0800

    throtled

--- system/services/power-profiles.nix
+++ system/services/power-profiles.nix
@@ -24,6 +24,22 @@ let
     cpupower frequency-set -g performance
   '';
 in {
+  services.throttled = {
+    enable = true;
+    extraConfig = ''
+      [UNDERVOLT]
+      # CPU core voltage offset (mV)
+      CORE: -105
+      # Integrated GPU voltage offset (mV)
+      GPU: -85
+      # CPU cache voltage offset (mV)
+      CACHE: -105
+      # System Agent voltage offset (mV)
+      UNCORE: -85
+      # Analog I/O voltage offset (mV)
+      ANALOGIO: 0
+    '';
+  };
   services.udev.extraRules = ''
     # start/stop services on power (un)plug
     SUBSYSTEM=="power_supply", ATTR{online}=="1", RUN+="${plugged}"
