bc44b6c0fa45d99d8a8434a1d6016fb017ba50a3
icey Fufezan <fufexan@protonmail.com>
2024-02-15 08:32:49 -0800

    Terminal: Git user

--- home/terminal/programs/git.nix
+++ home/terminal/programs/git.nix
@@ -51,7 +51,7 @@
 
     extraConfig.gpg.format = "ssh";
 
-    userEmail = "fufexan@protonmail.com";
-    userName = "icey Fufezan";
+    userEmail = "Icey-Glitch@riseup.net";
+    userName = "Icey-Glitch";
   };
 }
