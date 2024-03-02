168df4cd1373b961bb0c9c1ff5e628013b49c830
Icey-Glitch <Icey-Glitch@riseup.net>
2024-03-01 16:10:05 -0800

    fix desktop

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -37,13 +37,11 @@
       nvidiaSettings = true;
 
       # Optionally, you may need to select the appropriate driver version for your specific GPU.
-      package = config.boot.kernelPackages.nvidiaPackages.stable
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
     };
   };
 
   networking.hostName = "desktop";
-
-  xserver.videoDrivers = ["nvidia"];
 
   security.tpm2.enable = true;
 
