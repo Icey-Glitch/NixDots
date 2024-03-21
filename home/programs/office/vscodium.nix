5401c3ea03bf643b0b83c78489e42879a6cf02e7
Icey-Glitch <Icey-Glitch@riseup.net>
2024-03-21 08:48:03 -0700

    more codium

--- home/programs/office/vscodium.nix
+++ home/programs/office/vscodium.nix
@@ -1,17 +1,17 @@
 {
   pkgs,
-  config,
   inputs,
-  system,
   ...
 }: let
   extensions = inputs.nix-vscode-extensions.extensions.${pkgs.system};
 in {
+  home.sessionVariables.NIXOS_OZONE_WL = "1";
   programs.vscode = {
     enable = true;
     package = pkgs.vscodium;
     extensions = with extensions; [
       open-vsx.catppuccin.catppuccin-vsc
+      open-vsx.jnoortheen.nix-ide
     ];
   };
 }
