a5e9ab9a3ef052f132a0b34bb2bd693ebd57485e
Icey-Glitch <Icey-Glitch@riseup.net>
2024-04-08 07:48:37 -0700

    Nvidia fixes
    Signed-off-by: Icey-Glitch <Icey-Glitch@riseup.net>

--- system/hardware/nvidia-fixes.nix
+++ system/hardware/nvidia-fixes.nix
@@ -6,14 +6,26 @@
     WLR_NO_HARDWARE_CURSORS = "1";
     EGL_PLATFORM = "wayland";
     __GL_GSYNC_ALLOWED = "1";
-    __GL_VRR_ALLOWED = "0";
+    #__GL_VRR_ALLOWED = "0";
   };
 
+  boot.extraModprobeConfig = ''
+    options nvidia NVreg_RegistryDwords="PowerMizerEnable=0x1; PerfLevelSrc=0x2222; PowerMizerLevel=0x3; PowerMizerDefault=0x3; PowerMizerDefaultAC=0x3"
+  '';
+
   hardware.nvidia = {
+    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      version = "550.67";
+      sha256_64bit = "sha256-mSAaCccc/w/QJh6w8Mva0oLrqB+cOSO1YMz1Se/32uI=";
+      sha256_aarch64 = "sha256-+UuK0UniAsndN15VDb/xopjkdlc6ZGk5LIm/GNs5ivA=";
+      openSha256 = "sha256-M/1qAQxTm61bznAtCoNQXICfThh3hLqfd0s1n1BFj2A=";
+      settingsSha256 = "sha256-FUEwXpeUMH1DYH77/t76wF1UslkcW721x9BHasaRUaM=";
+      persistencedSha256 = "sha256-ojHbmSAOYl3lOi2X6HOBlokTXhTCK6VNsH6+xfGQsyo=";
+    };
     modesetting.enable = true;
     powerManagement.enable = true;
     open = false;
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.production;
+    #package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
 }
