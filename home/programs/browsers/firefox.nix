1b0d7373d363becb8b28a8e198a9ec0f222d84f7
Icey-Glitch <Icey-Glitch@riseup.net>
2024-04-20 16:12:27 -0700

    Move to github fetch for user.js config options through better fox
    Signed-off-by: Icey-Glitch <Icey-Glitch@riseup.net>

--- home/programs/browsers/firefox.nix
+++ home/programs/browsers/firefox.nix
@@ -21,14 +21,39 @@ in {
 
     profiles = {
       betterfox =
-        extramaker "betterfox"
-        // {
+        {
           isDefault = true;
           extensions = with firefoxExtensions; [
             nur.repos.rycee.firefox-addons.ublock-origin
           ];
           # someOption = "value";
-        };
+        }
+        // (
+          let
+            inherit (inputs.ludovico-nixpkgs.packages.${pkgs.system}) firefox-gnome-theme;
+            betterfox = pkgs.fetchFromGitHub {
+              owner = "yokoffing";
+              repo = "Betterfox";
+              rev = "122.1";
+              hash = "sha256-eHocB5vC6Zjz7vsvGOTGazuaUybqigODEIJV9K/h134=";
+            };
+          in {
+            extraConfig = ''
+              ${builtins.readFile "${betterfox}/Fastfox.js"}
+              ${builtins.readFile "${betterfox}/Peskyfox.js"}
+              ${builtins.readFile "${betterfox}/Securefox.js"}
+
+              /* Custom User.js */
+              user_pref("browser.tabs.firefox-view-next", false);
+              user_pref("privacy.sanitize.sanitizeOnShutdown", false);
+              user_pref("privacy.clearOnShutdown.cache", false);
+              user_pref("privacy.clearOnShutdown.cookies", false);
+              user_pref("privacy.clearOnShutdown.offlineApps", false);
+              user_pref("browser.sessionstore.privacy_level", 0);
+
+            '';
+          }
+        );
       # test = {
       #   id = 1;
       #  betterfox = {
