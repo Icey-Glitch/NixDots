218414c8e7024a466041329aa02c47f8826d3a8b
Icey-Glitch <Icey-Glitch@riseup.net>
2024-05-06 08:57:20 -0700

    bluetooth
    Signed-off-by: Icey-Glitch <Icey-Glitch@riseup.net>

--- system/hardware/bluetooth.nix
+++ system/hardware/bluetooth.nix
@@ -17,6 +17,15 @@
     };
   };
 
+  services.pipewire.wireplumber.extraConfig.bluetooth."51-bluez-config" = ''
+    bluez_monitor.properties = {
+    	["bluez5.enable-sbc-xq"] = true,
+    	["bluez5.enable-msbc"] = true,
+    	["bluez5.enable-hw-volume"] = true,
+    	["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag a2dp_sink a2dp_source]"
+    }
+  '';
+
   # https://github.com/NixOS/nixpkgs/issues/114222
   systemd.user.services.telephony_client.enable = false;
 }
