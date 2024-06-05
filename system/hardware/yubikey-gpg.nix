6b56554b0ee1d3e7407d560961b1e9e27419cdbb
Icey-Glitch <Icey-Glitch@riseup.net>
2024-06-05 15:01:05 -0700

    Yubikey: minor chages
    Signed-off-by: Icey-Glitch <Icey-Glitch@riseup.net>

--- system/hardware/yubikey-gpg.nix
+++ system/hardware/yubikey-gpg.nix
@@ -1,11 +1,4 @@
 {pkgs, ...}: {
-  programs.ssh.startAgent = false;
-
-  environment.systemPackages = with pkgs; [
-    gnupg
-    yubikey-personalization
-  ];
-
   environment.shellInit = ''
     gpg-connect-agent /bye
     export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
@@ -18,8 +11,24 @@
     pcscd.enable = true;
   };
 
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = true;
+  hardware.gpgSmartcards.enable = true;
+
+  programs = {
+    ssh.startAgent = false;
+    gnupg.agent = {
+      enable = true;
+      enableSSHSupport = true;
+    };
   };
+
+  environment.systemPackages = with pkgs; [
+    gnupg
+    # Yubico's official tools
+    yubikey-manager # cli
+    yubikey-manager-qt # gui
+    yubikey-personalization # cli
+    yubikey-personalization-gui # gui
+    yubico-piv-tool # cli
+    yubioath-flutter # gui
+  ];
 }
