7e92a9449c3443a5e4a2de7682147837cf36382b
Icey-Glitch <Icey-Glitch@riseup.net>
2024-06-06 16:54:21 -0700

    desktop: update nvidia drv
    Signed-off-by: Icey-Glitch <Icey-Glitch@riseup.net>

--- system/hardware/nvidia-fixes.nix
+++ system/hardware/nvidia-fixes.nix
@@ -40,7 +40,7 @@
       wlroots_0_16 = final.wlroots_0_16.overrideAttrs (_: {
         patches = [
           ./patches/wlroots-nvidia.patch
-          ./patches/wlroots-screenshare.patch
+          #  ./patches/wlroots-screenshare.patch
         ];
       });
     })
@@ -60,12 +60,12 @@
 
   hardware.nvidia = {
     package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-      version = "550.67";
-      sha256_64bit = "sha256-mSAaCccc/w/QJh6w8Mva0oLrqB+cOSO1YMz1Se/32uI=";
-      sha256_aarch64 = "sha256-+UuK0UniAsndN15VDb/xopjkdlc6ZGk5LIm/GNs5ivA=";
-      openSha256 = "sha256-M/1qAQxTm61bznAtCoNQXICfThh3hLqfd0s1n1BFj2A=";
-      settingsSha256 = "sha256-FUEwXpeUMH1DYH77/t76wF1UslkcW721x9BHasaRUaM=";
-      persistencedSha256 = "sha256-ojHbmSAOYl3lOi2X6HOBlokTXhTCK6VNsH6+xfGQsyo=";
+      version = "555.42.02";
+      sha256_64bit = "sha256-k7cI3ZDlKp4mT46jMkLaIrc2YUx1lh1wj/J4SVSHWyk=";
+      sha256_aarch64 = "sha256-ekx0s0LRxxTBoqOzpcBhEKIj/JnuRCSSHjtwng9qAc0=";
+      openSha256 = "sha256-3/eI1VsBzuZ3Y6RZmt3Q5HrzI2saPTqUNs6zPh5zy6w=";
+      settingsSha256 = "sha256-rtDxQjClJ+gyrCLvdZlT56YyHQ4sbaL+d5tL4L4VfkA=";
+      persistencedSha256 = "sha256-3ae31/egyMKpqtGEqgtikWcwMwfcqMv2K4MVFa70Bqs=";
     };
 
     modesetting.enable = true;
