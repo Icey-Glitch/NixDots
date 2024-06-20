da2f6b911c4fdab20a6e6fc6b580cb7445d9ba3f
Icey-Glitch <Icey-Glitch@riseup.net>
2024-06-20 07:42:54 -0700

    firefox
    Signed-off-by: Icey-Glitch <Icey-Glitch@riseup.net>

--- home/programs/browsers/firefox.nix
+++ home/programs/browsers/firefox.nix
@@ -28,6 +28,7 @@ in {
         "https://big.oisd.nl/"
         "http://sbc.io/hosts/hosts"
         "https://github.com/DandelionSprout/adfilt/raw/master/LegitimateURLShortener.txt"
+        "https://gist.githubusercontent.com/Icey-Glitch/d7e365b793bfae21759b750e316d3744/raw/63a1661f3c7e0aac85a8fbb9499fa305d4507e91/ytbetter.txt"
       ];
     in {
       "3rdparty".Extensions = {
@@ -72,6 +73,8 @@ in {
             sponsorblock
             clearurls
             old-reddit-redirect
+            youtube-redux
+            return-youtube-dislikes
             reddit-enhancement-suite
             darkreader
             fastforwardteam
@@ -137,6 +140,40 @@ in {
               user_pref("privacy.clearOnShutdown.cookies", false);
               user_pref("privacy.clearOnShutdown.offlineApps", false);
               user_pref("browser.sessionstore.privacy_level", 0);
+              user_pref("browser.newtabpage.activity-stream.system.showWeather", false);
+
+
+              // Disable Firefox Accounts & Sync
+              user_pref("identity.fxaccounts.enabled", false);
+              user_pref("identity.fxaccounts.remote.signup.uri", "");
+              user_pref("identity.fxaccounts.remote.signin.uri", "");
+              user_pref("identity.fxaccounts.remote.force_auth.uri", "");
+              user_pref("identity.mobilepromo.android", "");
+              user_pref("identity.mobilepromo.ios", "");
+
+              // Disable Firefox Sync
+              user_pref("services.sync.engine.bookmarks", false);
+              user_pref("services.sync.engine.history", false);
+              user_pref("services.sync.engine.passwords", false);
+              user_pref("services.sync.engine.prefs", false);
+              user_pref("services.sync.engine.tabs", false);
+              user_pref("services.sync.enabled", false);
+
+              // Disable sync setup from about:preferences
+              user_pref("services.sync.autoconnect", false);
+
+              // Disable Firefox Sync migration
+              user_pref("services.sync.prefs.sync.fxaccounts.enabled", false);
+              user_pref("services.sync.prefs.sync.fxaccounts.auth.uri", "");
+              user_pref("services.sync.prefs.sync.identity.fxaccounts.enabled", false);
+
+              // Disable other sync-related services
+              user_pref("services.sync.registerEngines", "");
+              user_pref("services.sync.username", "");
+              user_pref("services.sync.scheduler.idleInterval", 0);
+              user_pref("services.sync.scheduler.activeInterval", 0);
+              user_pref("services.sync.scheduler.immediateInterval", 0);
+              user_pref("services.sync.scheduler.singleDeviceInterval", 0);
 
 
             '';
