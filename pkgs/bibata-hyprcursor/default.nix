97ba03b0bf697db26d69aedbff80d1e96b4a1a0e
Icey-Glitch <Icey-Glitch@riseup.net>
2024-06-20 08:05:00 -0700

    fix cursor
    Signed-off-by: Icey-Glitch <Icey-Glitch@riseup.net>

--- pkgs/bibata-hyprcursor/default.nix
+++ pkgs/bibata-hyprcursor/default.nix
@@ -10,7 +10,7 @@ stdenvNoCC.mkDerivation (_finalAttrs: {
   inherit (bibata-cursors) version;
 
   src = fetchurl {
-    url = "https://cdn.discordapp.com/attachments/1216066899729977435/1216076659149504643/HyprBibataModernClassicSVG.tar.gz?ex=666fcae5&is=666e7965&hm=1c33b73c3d19cf316a4216d88279be3081715676c31aed7db02fa7a8aa3f90ce&";
+    url = "https://cdn.discordapp.com/attachments/1216066899729977435/1216076659149504643/HyprBibataModernClassicSVG.tar.gz?ex=667510e5&is=6673bf65&hm=e87636083ccb98d4ac6d6e815a9ea853bb958ccc80a83997a53127645fd9ac1b&";
     name = "HyprBibataModernClassic.tar.gz";
     hash = "sha256-KDYoULjJC0Nhdx9Pz5Ezq+1F0tWwkVQIc5buy07hO98=";
   };
