a0584274782f4c2abe0b480a7db5ff952c3272bd
Icey-Glitch <Icey-Glitch@riseup.net>
2024-09-03 07:38:47 -0700

    Disable tlp
    Signed-off-by: Icey-Glitch <Icey-Glitch@riseup.net>

--- system/services/power-profiles.nix
+++ system/services/power-profiles.nix
@@ -86,10 +86,10 @@ _:
 
   powerManagement.powertop.enable = true;
   services.tlp = {
-    enable = true;
-    extraConfig = ''
-      CPU_SCALING_GOVERNOR_ON_AC=performance
-      CPU_SCALING_GOVERNOR_ON_BAT=powersave
-    '';
+    enable = false;
+    Settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+    };
   };
 }
