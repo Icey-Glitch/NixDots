a697312ffd90c2af19ee27e7a98700b07c62721c
Icey-Glitch <Icey-Glitch@riseup.net>
2025-01-06 08:01:30 -0800

    update bl stuff

--- system/hardware/bluetooth.nix
+++ system/hardware/bluetooth.nix
@@ -17,14 +17,14 @@
     };
   };
 
-  services.pipewire.wireplumber.extraConfig.bluetooth."51-bluez-config" = ''
-    bluez_monitor.properties = {
-    	["bluez5.enable-sbc-xq"] = true,
-    	["bluez5.enable-msbc"] = true,
-    	["bluez5.enable-hw-volume"] = true,
-    	["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag a2dp_sink a2dp_source]"
-    }
-  '';
+  services.pipewire.wireplumber.extraConfig.bluetoothEnhancements = {
+    "bluez_monitor.properties" = {
+      "bluez5.enable-sbc-xq" = true;
+      "bluez5.enable-msbc" = true;
+      "bluez5.enable-hw-volume" = true;
+      "bluez5.headset-roles" = ["hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag" "a2dp_sink" "a2dp_source"];
+    };
+  };
 
   systemd.user.services.mpris-proxy = {
     description = "Mpris proxy";
